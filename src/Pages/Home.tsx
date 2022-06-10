19656f7bc4af6e02f39950c51d1cf62374afead1
YNeKIT <97906064+YNeKIT@users.noreply.github.com>
2022-06-10 16:24:54 +0300

    init repository

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// export interface IHomepageProps {};
+
+// const HomePage: React.FunctionComponent<IHomepageProps> = (props) => {
+//     return <div>
+//        <Link to="<p> Pagina principala </p>" />
+//         </div>;
+// };
+// export default HomePage;
